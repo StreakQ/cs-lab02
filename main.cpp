e1af6e8e077b1e8bd402d07c4c9ec6a59ffb4e4d
Alice (KovalevEO) <KovalevEO@mpei.ru>
2021-03-22 16:14:34 +0300

    code:add find max

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main()
 {
     cout<<"Enter a and B ";
-    int a,b;
+    int a,b,max,min;
    cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+     max=a*b;
+
+    cout<<"max="<<a*b<< '\n';
+        
     return 0;
 }
